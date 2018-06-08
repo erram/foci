835a99bc3364afa846b4d8a4c252f1ee784bd866
Richard Molnar <Richard_Molnar@epam.com>
2018-06-08 16:13:11 +0200

    Players

--- app/models/Players.js
+++ app/models/Players.js
@@ -4,14 +4,14 @@ var bcrypt = require('bcrypt-nodejs');
 
 // define the schema for our user model
 var playerSchema = mongoose.Schema({
-    country: String,
-    position: String,
-    name: String,
-    born: String,
-    selected: Number,
-    goals: Number,
-    team: String,
-    point: Number
+    ORSZÁG: String,
+    POZICIO: String,
+    NÉV: String,
+    SZÜLETETT: String,
+    VÁLOGATOTTSÁG: Number,
+    GÓLOK: Number,
+    CSAPAT: String,
+    ÉRTÉK: Number
 });
 
 // create the model for users and expose it to our app
--- app/routes/players.js
+++ app/routes/players.js
@@ -1,2 +1,16 @@
 var app = require('../../app')
 var isLoggedIn = require('../utils/auth')
+var Players = require('../models/Players')
+
+app.get("/teambuild" ,function(req, res){
+    Players.find({}, function(err, players){
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            res.render("players.ejs", {
+                user: req.user,
+                players: players
+            })
+        }
+    })
+})
